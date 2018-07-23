4152c2d1e910b59c67bd3599f4c1506dbfce1cc8
xiewei <cyerogy@163.com>
2018-07-23 11:33:42 +0800

    slider uploader

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -34,6 +34,26 @@
       <div class="weui-cells weui-cells_after-title">
         <checkgroup :checkboxItems="cItems" @importCheckData="getCheckResult"></checkgroup>
       </div>
+
+      <div class="weui-cells__title">开关</div>
+      <div class="weui-cells weui-cells_after-title">
+        <div class="weui-cell weui-cell_switch">
+          <div class="weui-cell__bd">标题文字</div>
+          <div class="weui-cell__ft">
+            <switch checked @change = "switchChange"/>
+          </div>
+        </div>
+      </div>
+
+      <div class="weui-cells__title">slider</div>
+      <div class="weui-cells weui-cells_after-title">
+         <slider show-value @change="sliderChange"/>
+      </div>
+
+      <div class="weui-cells__title">上传组件</div>
+      <div class="weui-cells weui-cells_after-title">
+         <upload></upload>
+      </div>
     </div>
   </div>
 </template>
@@ -43,8 +63,9 @@
 import base64 from "../../../src/images/base64";
 import radiogroup from "@/components/radio.vue";
 import checkgroup from "@/components/check.vue";
+import upload from "@/components/upload.vue";
 export default {
-  components: { radiogroup, checkgroup },
+  components: { radiogroup, checkgroup, upload },
   data() {
     return {
       icon: "",
@@ -69,6 +90,12 @@ export default {
     getCheckResult: function() {
       let args = Array.from(arguments);
       console.log(args);
+    },
+    switchChange: function(e) {
+      console.log("switch发生change事件，携带value值为：" + e.mp.detail.value);
+    },
+    sliderChange: function(e) {
+      console.log("slider3 发生 change 事件，携带值为" + e.mp.detail.value);
     }
   }
 };
